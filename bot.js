3e6ea8bcf826ea749933c1bc4eff7fd35de8de37
Max <e-max@bk.ru>
2025-04-23 23:30:12 +0300

    added hello

--- bot.js
+++ bot.js
@@ -4,13 +4,14 @@ const token = process.env.SECRET_KEY;
 
 const bot = new TelegramBot(token, { polling:true });
 
-bot.onText(/\/jstatus/, async (msg) => {
+bot.onText(/\/status/, async (msg) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Привет! Я Jangseung');
+    bot.sendMessage(chatId, 'Привет! Я Jangseung, я тотем этой группы. Я отпугиваю злых духов и приманиваю хороших участников');
+    console.log(`${msg}`);
 });
 
 bot.on('polling_error', (error) => {
     console.error('Polling error:', error);
   });
   
-console.log('Бот запущен...');
+console.log('Bot started...');
