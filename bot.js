2a2e7be8c6b2f0165d34d22c5bca16b1f313873a
Max <e-max@bk.ru>
2025-04-24 01:22:06 +0300

    teached bot to greating in different languages

--- bot.js
+++ bot.js
@@ -4,15 +4,75 @@ const token = process.env.SECRET_KEY;
 
 const bot = new TelegramBot(token, { polling:true });
 
+bot.on('new_chat_members', (msg) => {
+    const chatId = msg.chat.id;
+    const newMembers = msg.new_chat_members;
+
+    newMembers.forEach(member => {
+        var name;
+        var greeting;
+        var firstName = member.first_name;
+        var lastName = member.last_name;
+        var username = member.username;
+        var id = member.id;
+        var language = member.language_code;
+        
+        // firstName && lastName   ? name = `${firstName} ${lastName}` 
+        //     : firstName         ? name = firstName 
+        //     : lastName          ? name = lastName 
+        //     :                     name = member.id
+
+        if (firstName && lastName){
+            name = `${firstName} ${lastName}`;
+        } else if (firstName){
+            name = firstName;
+        } else if (lastName){
+            name = lastName;
+        } else if (username){
+            name = username;
+        } else {
+            name = `id ${id}`;
+        }
+
+        switch (language) {
+            case "ru":
+            case "ru-ru":
+                greeting = 'Привет';
+                break;
+            case 'en':
+            case 'en-us':
+            case 'en-gb':
+                greeting = 'Hello!';
+                break;
+            case 'ar':
+                greeting = 'مرحبا!';
+                break;
+            case 'zh':
+                case 'zh-cn':
+                greeting = '你好!';
+                break;
+            case 'ko':
+            case 'ko-kr':
+                greeting = '안녕하세요!';
+                break;
+            default:
+                greeting = 'Привет! Hi! 안녕하세요';
+        }
+
+        var welcomeMessage = `${greeting} ${name} \n\n🗣: 🇷🇺🇬🇧🇰🇷`;
+
+        bot.sendMessage(chatId, welcomeMessage);
+    })
+});
+
 bot.onText(/\/status/, async (msg) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Привет! Я Jangseung, я тотем этой группы. Я отпугиваю злых духов и приманиваю хороших участников');
+    bot.sendMessage(chatId, 'Привет! Я Jangseung - тотем этой группы. Я отпугиваю злых духов и приманиваю хороших участников');
     console.log(`${msg}`);
 });
 
 bot.on('polling_error', (error) => {
-    var t = `${new Date.toGMTString()}`;
-    console.error(`${t} Polling error:`, error);
+    console.error(`${new Date.toGMTString()} Polling error:`, error);
   });
   
-console.log('Bot started...');
+console.log(`${new Date.toGMTString()} Bot started...`);
